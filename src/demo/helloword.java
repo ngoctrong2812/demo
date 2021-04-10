ed2d9663776f6eae59ef3772bf45ef470141b6d0
DESKTOP-T5FSIUL\ACER A315 <ngoctuyen122889@gmail.com>
2021-04-10 11:52:24 +0700

    demo

---
+++ src/demo/helloword.java
@@ -0,0 +1,18 @@
+package demo;
+
+import java.util.Scanner;
+
+public class helloword {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap a :");
+		int a = sc.nextInt();
+		System.out.print("Nhap b :");
+		int b = sc.nextInt();
+		System.out.println("Ket qua : " + (a+b));
+	}
+
+}
